70b6b36af357de0be108439b302745bc75a92eef
Dennis Markgraf <dennis.markgraf@hhey.studio>
2017-08-07 18:27:25 +0200

    Updated inject task

--- tasks/inject.js
+++ tasks/inject.js
@@ -6,12 +6,15 @@ const File = require('./../data/file')
 const defaults = {
   color: 'yellow',
   clean: false,
+  dataUrl: ['jpg', 'jpeg', 'png', 'gif'],
   dest: '',
   base: '',
   handover: {}
 }
 
 function _regEx (type) { return new RegExp(String.raw`([^\S\n]*)(<!-{2,}\s*inject:${type}\s*-{2,}>)[\S\s]*?(\s*<!-{2,}\s*endinject\s*-{2,}>)`) }
+
+function _regExFile (path) { return new RegExp(String.raw`([^\S\n]*)(<!-{2,}\s*inject:${path}\s*-{2,}>)`, 'g') }
 
 function _add (injections, type, data) {
   if (!injections[type]) {
@@ -22,6 +25,8 @@ function _add (injections, type, data) {
 }
 
 async function inject (data, task) {
+  // todo: use magic-string https://github.com/Rich-Harris/magic-string
+  // todo: inject to multiple files
   let config = task.config
   let {files, matches} = await Files.factory(data, task)
   let injectionFile = matches
@@ -32,10 +37,15 @@ async function inject (data, task) {
     for (let file of files) {
       let type = path.extname(file.path).replace('.', '')
       let regExp = _regEx(type)
+      let rexExpFile = _regExFile(file.path)
 
       let filePath = config.base ? path.relative(config.base, file.path) : file.path
 
-      if (regExp.test(injectionFile._data)) {
+      if (rexExpFile.test(injectionFile._data)) {
+        let asDataUrl = task.config.dataUrl.indexOf(file.ext) !== -1
+        injectionFile.data = injectionFile._data.replace(rexExpFile, `$1${asDataUrl ? file.dataUrl : file.data}`)
+        task.log(`${injectionFile.path}: injected file ${asDataUrl ? 'data url' : 'contents'} ${file.path}`.gray, null)
+      } else if (regExp.test(injectionFile._data)) {
         switch (type) {
           case 'js':
             _add(injections, type, `<script src="/${filePath}"></script>`)
